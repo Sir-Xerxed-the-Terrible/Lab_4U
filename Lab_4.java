09d90e65f4b4d813b1d91e3ed93b9697b2f1ebda
Sir-Xerxed-the-Terrible <xerxed123@gmail.com>
2025-09-22 14:11:23 +0800

    Add files via upload
    This is the revised version, Sir. I just found out that my activity 2 was lacking some lines to comply with the requirement for activity 2.

--- Lab_4.java
+++ Lab_4.java
@@ -21,8 +21,8 @@ public class Lab_4 {
         Scanner input = new Scanner(System.in);
         System.out.println("Java Loop Laboratory Activity 4 \n\nof \n\nMarcelo Jr L. Depalubos\n\nPlease input a number to start.\n");
         int choice;
-        
-        do{
+
+        do {
             System.out.println("1. Activity I");
             System.out.println("2. Activity II");
             System.out.println("3. Activity III");
@@ -31,79 +31,84 @@ public class Lab_4 {
             System.out.println("6. Close");
             System.out.println("\nPlease input a number to choose an activity.");
             choice = input.nextInt();
-            
-        switch (choice) {
-            case 1: {
-        System.out.println("\nPlease enter an Integer.\n");
-        int Integer = input.nextInt();
-        for (int i= 1; i <= 10; i++){
-        System.out.printf("%d * %d = %d \n",Integer ,i, Integer*i);}break;
+
+            switch (choice) {
+                case 1: {
+                    System.out.println("\nPlease enter an Integer.\n");
+                    int Integer = input.nextInt();
+                    for (int i = 1; i <= 10; i++) {
+                        System.out.printf("%d * %d = %d \n", Integer, i, Integer * i);
+                    }
+                    break;
+                }
+                case 2: {
+                    System.out.println("\nPlease enter an Integer(N).");
+                    // Assuming 'n' is the target number (e.g., 10 for sum of 1 to 10)
+                    int n = input.nextInt();
+                    int sum = 0;
+                    int i = 1;
+
+                    while (i <= n) {
+                        sum += i;
+                        i++;
+                    System.out.println("N=" + sum);
+                    }
+                    break;
+                }
+                case 3: {
+                    System.out.println("\nPlease enter an Integer (n).");
+                    int n = input.nextInt();
+                    int factorial = 1;
+
+                    do {
+                        factorial *= n;
+                        n--;
+                    } while (n >= 1);
+                    System.out.println("Factorial: " + factorial);
+                }
+                break;
+                case 4: {
+                    System.out.println("\nPlease enter a Number(x).");
+                    int number = input.nextInt();
+
+                    System.out.println("Even numbers: ");
+                    for (int i = 0; i <= number; i += 2) {
+                        System.out.println(i);
+
+                    }
+                    System.out.println("For Odd numbers: ");
+                    for (int x = 1; x <= number; x += 2) {
+                        System.out.println(x);
+                    }
+                    break;
+                }
+                case 5: {
+                    int Numreversi = 0;
+                    System.out.println("\nPlease enter an Integer (z) to reverse. ");
+                    int Integer = input.nextInt();
+
+                    System.out.println("Reversed of the Integer: ");
+                    for (int i = Integer; i != 0; i /= 10) {
+                        int digit = i % 10;
+                        Numreversi = Numreversi * 10 + digit;
+                    }
+
+                    System.out.println(Numreversi);
+                    break;
+
+                }
+                case 6: {
+                    System.out.println("\nThank You and Good Bye.");
+                    System.exit(0);
+                }
+                default:
+                    System.out.println("\nInvalid Choice");
             }
-            case 2: {
-        System.out.println("\nPlease enter an Integer(N).");
-        int N = 0;
-        int sum=0;
-        N = input.nextInt();
-        while (N >= 0){
-            sum=sum + N;
-        System.out.println("N= "+ N);
-        N = N - 1;
-            }
-        System.out.println(sum);break;
-            }
-            case 3: {
-        System.out.println("\nPlease enter an Integer (n).");
-        int n = input.nextInt();
-        int factorial = 1;
-        
-        
-        
-        do {factorial *= n; n--;
-        } 
-        while (n >= 1);
-        System.out.println("Factorial: " + factorial);
-            }break;
-            case 4: {
-        System.out.println("\nPlease enter a Number(x).");
-             int number = input.nextInt();
-            
-        System.out.println("Even numbers: ");
-             for (int i = 0 ; i <= number; i += 2 )
-              {System.out.println(i);
-                 
-             }   
-        System.out.println("For Odd numbers: ");
-            for (int x = 1; x <= number; x += 2)
-             {
-                System.out.println(x);
-           }break;
-            }
-            case 5: {
-             int Numreversi = 0;   
-        System.out.println("\nPlease enter an Integer (z) to reverse. ");
-             int Integer = input.nextInt();
-        
-        System.out.println("Reversed of the Integer: ");
-             for (int i = Integer; i != 0; i /= 10){
-                 int digit = i % 10;
-                 Numreversi = Numreversi * 10 + digit;} 
-             
-             
-             System.out.println(Numreversi);break;
-             
-            }
-            case 6: {
-        System.out.println("\nThank You and Good Bye.");
-        System.exit(0);
-            }
-             default: System.out.println("\nInvalid Choice");
-        }
-                    
-                    
-        } while (choice !=0);
-        
+
+        } while (choice != 0);
+
         input.close();
-       
+
     }
-    
+
 }
